1b6f5f5392a10d8d558125b2d4891aae8716f10a
thebabalola <t.babalolajoseph@gmail.com>
2025-12-12 04:05:38 +0100

    Add ERC20 token interface imports for reward system

--- contracts/habitrac/contracts/HabiTrac.sol
+++ contracts/habitrac/contracts/HabiTrac.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract HabiTrac is Ownable {
     struct Habit {
